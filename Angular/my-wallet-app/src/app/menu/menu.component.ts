48693f4bffd57e81018760b9c38da1efeeaef5d8
trololog <42019422+trololog@users.noreply.github.com>
2019-10-10 10:57:21 -0500

    Fixing styles
    Fixing styles

--- Angular/my-wallet-app/src/app/menu/menu.component.ts
+++ Angular/my-wallet-app/src/app/menu/menu.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  menuClass:string = "";
+  menuOpened:boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  openMenu() {
+    if(this.menuOpened) {
+      this.menuClass = 'menu-hidden';
+    } else {
+      this.menuClass = 'menu-list menu-visible';
+    }
+
+    this.menuOpened = !this.menuOpened;
+  }
 }
